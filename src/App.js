bda0033319263183b6b5038539cc64d8d744edda
Jeremy J. Johnson <jeremyjohnson7@mail.weber.edu>
2017-04-11 10:56:27 -0600

    API call

--- src/App.js
+++ src/App.js
@@ -4,7 +4,29 @@ import './App.css';
 
 import Recipe from './Recipe';
 
+const pancakes = {
+    "_id": "6aebfaf1-3cef-4d60-8375-942e8ca4e44e",
+    "title": "Pancakes & Waffles",
+    "ingredients": [
+        "2 cups flour",
+        "2 Tbsp. sugar",
+        "1 Tbsp. baking powder",
+        "2 tsp. salt",
+        "2 eggs",
+        "1 1/2 cups milk",
+        "1/3 cups oil"
+    ],
+    "user": "jjj"
+}
+
 class App extends Component {
+    componentDidMount() {
+        fetch('http://localhost:3080/api/recipes/f5642476-f2c3-4fff-9a94-bd98c82da675?75816cb1a27029855a6c112658832c67c2b79efcd5238b8f53a3d96cbcb64b55')
+            .then(res => res.json())
+            .then(res => console.log(res))
+            .catch();
+    }
+
     render() {
         return (
             <div className="App">
@@ -15,7 +37,7 @@ class App extends Component {
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
-                <Recipe recipe={{"_id":"6aebfaf1-3cef-4d60-8375-942e8ca4e44e","title":"Pancakes & Waffles","ingredients":["2 cups flour","2 Tbsp. sugar","1 Tbsp. baking powder","2 tsp. salt","2 eggs","1 1/2 cups milk","1/3 cups oil"],"user":"jjj"}} editHandler={()=>1} />
+                <Recipe recipe={pancakes} editHandler={()=>1} />
             </div>
         );
     }
